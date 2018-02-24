01ec72c1de90e35d560a5dc9f6807a4c76cc6a45
Leonidas Tsaousis <leo.tsaou@gmail.com>
2018-02-24 23:30:57 +0200

    Found magic way to encapsulate both color AND getattr ops AND auto-types
    Go ahead, try it without --json

--- thrift-inspector.py
+++ thrift-inspector.py
@@ -2,18 +2,92 @@
 import argparse
 import json
 import sys
-from io import StringIO
-
+import termcolor
+from string import Formatter
 from thrift_tools.thrift_message import ThriftMessage
+
+
+
+class ColorFormatter(Formatter):
+    def __init__(self, msg_dict, nocolor):
+        Formatter.__init__(self)
+        self.msg_dict = msg_dict
+        self.nocolor = nocolor
+
+    def get_value(self, key, args, kwargs):
+        """
+        key is index (of args) or string (of kwargs)
+        Will return Null if not string!!
+        """
+        colored = ''
+        if isinstance(key, str):
+            if key=='':
+                return Formatter.get_value(self,key, args, kwargs)
+
+            # Check explicitly passed positional arguments first
+            # like .format('one', 'two')
+            try:
+                colored = kwargs[key]
+            except KeyError:
+                colored = self.msg_dict[key]
+
+        elif isinstance(key,int) or isinstance(key,long):
+            # Check explicitly passed named args then
+            # like .format(first='Hodor', last='Hodor!')
+            # or   .format(**data) with data={'first':'Hodor','last':'Hodor!'}
+            colored = args[key]
+
+        return colored
+
+
+
+
+    def format_field(self, value, format_spec):
+        if self.nocolor:
+            return format(value, format_spec)
+        else:
+            if format_spec:
+                value = self.colorize(str(value), format_spec)
+                return value
+            else:
+                return format(value, format_spec)
+
+
+    def colorize(self, value, rule):
+        """
+        :param value: must be str
+        :param rule: a single character, one of {'b','g', 'y', 'm', 'r', 'w', 'e', 'c', B'}
+        :return: a termcolor.colorize() transformation upon value, according to the rule
+        """
+        if rule=='b':
+            return termcolor.colored(value, 'blue')
+        elif rule=='g':
+            return termcolor.colored(value, 'green')
+        elif rule=='y':
+            return termcolor.colored(value, 'yellow')
+        elif rule=='m':
+            return termcolor.colored(value, 'magenta')
+        elif rule=='r':
+            return termcolor.colored(value, 'red')
+        elif rule=='w':
+            return termcolor.colored(value, 'white')
+        elif rule=='c':
+            return termcolor.colored(value, 'cyan')
+        elif rule=='e': # grey
+            return termcolor.colored(value, None, attrs=['dark'])
+        elif rule=='B':
+            return termcolor.colored(value, None, attrs=['bold'])
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Deserialize Thrift payloads. Only TBinary currently supported")
                                     # epilog="See README for more on the options provided here")
-    #parser.add_argument("-i", "--in", help="read binary Thrift message from file", required=False)
     parser.add_argument('infile', help="read thrift message from binary file (omit for stdin)",
                         nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument("-d", "--debug", help="more verbose output", action="store_true")
     parser.add_argument("-c", "--nocolor", help="turnoff color outout", action="store_true")
+    parser.add_argument("--json", help="json output, no colors", action="store_true")
     args = parser.parse_args()
 
     data = []
@@ -36,4 +110,76 @@ if __name__ == "__main__":
 
         break
 
-    print json.dumps(msg.as_dict,indent=2)
+    # now we have the parsed Thrift message in 'msg'
+    """
+    msg.as_dict = {
+        "args": {
+            "fields": [
+                {
+                    "field_type": "i32",
+                    "field_id": 1,
+                    "value": 1
+                },
+                {
+                    "field_type": "struct",
+                    "field_id": 2,
+                    "value": {
+                        "fields": [
+                            {
+                                "field_type": "i32",
+                                "field_id": 1,
+                                "value": 15
+                            },
+                            {
+                                "field_type": "i32",
+                                "field_id": 2,
+                                "value": 10
+                            },
+                            {
+                                "field_type": "i32",
+                                "field_id": 3,
+                                "value": 2
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        "header": null,
+        "length": 54,
+        "seqid": 0,
+        "type": "call",
+        "method": "calculate"
+    }
+    """
+
+    # TODO isolate colorprinting code as Parser
+    # TODO test with Binary, Containers, Double, separate String, i64, byte, bool
+    # TODO isolate in function the Exception heuristic
+
+
+
+    if args.json:
+        print json.dumps(msg.as_dict,indent=2)
+    else:
+        # d = MessageDict(msg.as_dict)
+
+        colorFormatter = ColorFormatter(msg.as_dict, args.nocolor)
+        print colorFormatter.format('{type:B} "{method:w}" ({length} bytes) hdr:{header} seqid:{seqid}\nargs:')
+
+        for idx in range(len(msg.as_dict['args']['fields'])):
+            f = msg.as_dict['args']['fields'][idx]
+
+            field_fmt_str = "args[fields]["+str(idx)+"]"
+            # format_str = "{"+field_fmt_str+"[field_id]} <{"+field_fmt_str+"[field_type]}> value_placeholder"   #WORKS
+
+            # format_str = "{0:.5}{"+field_fmt_str+"[field_id]} <{"+field_fmt_str+"[field_type]}> value_placeholder" # WORKS
+            #       ...with the print colorFormatter.format(format_str, "to_be_fmted") invocation. Does limit the arg passed.
+
+            format_str = "{" + field_fmt_str + "[field_id]:b} <{" + field_fmt_str + "[field_type]}> value_placeholder"  # WORKS
+            print colorFormatter.format(format_str)
+
+
+
+            # format_str = "{field_id} <{field_type}> value_placeholder"
+            # colorFormatter.format(format_str, **f)
