ce1f8ceab041c9654f8ecaf525ae48de377a68ff
Leonidas Tsaousis <leonidas.tsaousis@circles.bz>
2018-02-27 09:29:30 +0200

    non-auto-casted ThriftStructs threw TypeError in json.dumps() : FIXED
    At some point deep in the ThriftMessage, some fields were not cast to native Python objects like dicts,
    remaining ThriftStructs. This could not be parsed by the default json.JSONEncoder.

--- thrift-inspector.py
+++ thrift-inspector.py
@@ -4,7 +4,7 @@ import json
 import sys
 import termcolor
 from string import Formatter
-from thrift_tools.thrift_message import ThriftMessage
+from thrift_tools.thrift_message import ThriftMessage, ThriftStruct
 
 
 
@@ -103,28 +103,14 @@ class ColorFormatter(Formatter):
 
 class ThriftJsonEncoder(json.JSONEncoder):
     def default(self, o):
-        """Implement this method in a subclass such that it returns
-        a serializable object for ``o``, or calls the base implementation
-        (to raise a ``TypeError``).
-
-        For example, to support arbitrary iterators, you could
-        implement default like this::
-
-            def default(self, o):
-                try:
-                    iterable = iter(o)
-                except TypeError:
-                    pass
-                else:
-                    return list(iterable)
-                # Let the base class default method raise the TypeError
-                return JSONEncoder.default(self, o)
-
         """
-        return iter(o.as_dict())
-
-
-
+        Default handler overriden to support Thrift Structs,
+        returning the dictified version of it by using it's as_dict builtin property
+        """
+        if isinstance(o, ThriftStruct):
+            return o.as_dict
+        else:
+            return super.default(self,o)
 
 
 
@@ -233,13 +219,6 @@ if __name__ == "__main__":
 
     if args.json:
         print json.dumps(msg.as_dict, indent=2, cls=ThriftJsonEncoder)
-        """
-        TODO fixme
-        When collections like sets are found, JSONB encoder can't parse them (not supported)
-        
-        Override json/encoder/default() to support set() or ThriftStruct
-        """
-
     else:
         colorFormatter = ColorFormatter(msg.as_dict, args.nocolor)
         print colorFormatter.format('{type:B} "{method:w}" ({length} bytes) hdr:{header} seqid:{seqid}\nargs:')
